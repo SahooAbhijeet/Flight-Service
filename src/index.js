ec35763b25509170456338d9ec9037c69b5542a0
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-09-24 17:36:04 +0530

    added basic structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const { ServerConfig } = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Successfully started the server on PORT : ${PORT}`);
+});
