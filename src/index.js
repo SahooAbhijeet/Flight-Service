fcf1ea3b62269e51867ae1e5d44f30cbea1c741c
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-12-27 10:11:23 +0530

    removed unnecessary file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
-app.use('/flightsService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
